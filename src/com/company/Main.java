fbda39561666486d04181aeed218569943022db3
zolotarevava <zolotareva.veronica@yandex.ru>
2017-12-12 10:14:57 +0300

    optimization

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,27 +12,23 @@ public class Main {
         File f = new File(path);
         //проверка на существование указанного каталога
         if (f.exists()) {
-            list(path);
+            readDir(f);
         }
         else System.out.println("\nNot directory: " + path);
     }
 
-    static  void  list(String path) {
-        File f = new File(path);
-        String[] sDirList = f.list();
-        //обработка содержимого каталога
-        for (int i = 0; i < sDirList.length; i++) {
-            //для каждого элемента массива создаем объект класса File и проверяем, соответствует ли он файлу или каталогу
-            File f1 = new File(path + File.separator + sDirList[i]);
-            if (f1.isFile())
-                System.out.println(path + File.separator + sDirList[i]);
-            else
-                //если каталог, рекурсивный вызоы метода list
-                list(path + File.separator + sDirList[i]);
-
+    public static void readDir(File directory){
+        if (directory.isDirectory()){
+            for (File file : directory.listFiles()) {
+                if(file.isFile()){
+                    System.out.println("\t" + file.getName());
+                }else {
+                    System.out.println("-dir- " + file.getName());
+                    readDir(file);
+                }
+            }
         }
-
-
     }
+
 
 }
